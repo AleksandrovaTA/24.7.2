daa253c6e47c55d03d1f8fd16d9a1c656d8224a4
AleksandrovaTA <118689430+AleksandrovaTA@users.noreply.github.com>
2023-03-06 22:27:21 +0300

    Update api.py

--- api.py
+++ api.py
@@ -1,4 +1,16 @@
- def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
+import json
+import requests
+from requests_toolbelt.multipart.encoder import MultipartEncoder
+
+
+class PetFriends:
+    """API-библиотека к веб-приложению 'Pet Friends'"""
+
+    def __init__(self):
+        self.base_url = "https://petfriends.skillfactory.ru/"
+        """Магическая функция '__init__' присваивает всем экземплярам класса 'PetFriends' базовый URL"""
+
+def add_new_pet_without_photo(self, auth_key: json, name: str, animal_type: str, age: str) -> json:
         """Функция добавляет информацию о новом питомце без фото и возвращает информацию в формате JSON"""
 
         headers = {'auth_key': auth_key['key']}
